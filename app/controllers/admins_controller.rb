00d27836679d05dba2c9641741080fe8ceac6e76
khanh-devos <dreamproperty.khanh@gmail.com>
2024-09-09 20:13:17 +0700

    Fix error Mass-assignment in Admin controller

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -65,6 +65,6 @@ class AdminsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def admin_params
-      params.require(:admin).permit(:username, :role, :permissions, :user_id)
+      params.require(:admin).permit(:username, :permission)
     end
 end
